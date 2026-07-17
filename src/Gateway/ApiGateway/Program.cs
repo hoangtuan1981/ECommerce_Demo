e99d31169809ceaa1cf005812da4c998a56dc7c8
Tuan Le Anh Hoang <tuan.leanhhoang@hitachids.com>
2026-07-17 15:19:54 +0700

    Add descriptions.

--- src/Gateway/ApiGateway/Program.cs
+++ src/Gateway/ApiGateway/Program.cs
@@ -14,10 +14,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("GatewayCors", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("http://localhost:5173") // Domain của ReactJS Client
             .AllowAnyHeader()
             .AllowAnyMethod()
-            .AllowCredentials();
+            .AllowCredentials();// Bắt buộc: Cho phép gửi và nhận Cookie chéo domain
     });
 });
 
@@ -35,28 +35,11 @@ if (app.Environment.IsDevelopment())
  */
 //app.UseHttpsRedirection();
 
-#region "old code"
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//};
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast =  Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast");
-#endregion "old code"
 
 app.UseCors("GatewayCors");
+
+//app.UseRouting();
+
 //use Yarp
 app.MapReverseProxy();
 
